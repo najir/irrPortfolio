5f1df21340ef4ac7dd635e978c27e69b705ec734
Najir <irrperks@gmail.com>
2023-03-12 23:19:08 -0700

    Set up basic table chart for some statistics, examples used is projects completed. Made a basic pie chart with some simple data next to it to display.

--- main/src/irrbackend/clientapp/src/components/GitFetch.js
+++ main/src/irrbackend/clientapp/src/components/GitFetch.js
@@ -8,8 +8,17 @@ class GitFetch extends React.Component{
     this.exampleData = [
       0,1,2,3,4,5,6,7,8,9,10,11,1,2,3,4,5,6,2,0,0,0,2,1,4,5,6,7,0,8,7,5
     ];
-    this.projects = 12;
-    this.updates = 15;
+    this.projectData = [
+      0,2,3,7,6
+    ];
+    this.languageData = [
+      {name: "cpp",
+       amount: 5},
+       {name: "c#",
+       amount: 5},
+       {name: "python",
+       amount: 5}
+    ];
     this.element = createRef();
     this.onScroll = this.onScroll.bind(this);
     this.setScroll = this.setScroll.bind(this);
@@ -51,17 +60,40 @@ class GitFetch extends React.Component{
                   <h5 id="textboxmain" className="ms-2">Performance and Activity</h5>
                 </div>
                 <p className="mt-5 mb-5">A small visualization of my recent personal acitivty</p>
+
+
                 <div className="data-wrapper">
-                  <p>Commits 30/Days</p>
-                  <div className="data-box">
-                    {this.exampleData.map((key)=>{
-                      return <div className="data-object" style={{opacity: (1.1-1/(key+1))}} ></div>
-                    })}
+                  <div className="commit-list">
+                    <p>Commits 30/Days</p>
+                    <div className="commit-box">
+                      {this.exampleData.map((key)=>{
+                        return <div className="data-object" style={{opacity: (1.1-1/(key+1))}} ></div>
+                      })}
+                    </div>
                   </div>
-                </div>
-                <div className="activity-widget">
-                  <div className="chartObject" style={{width: "test"}}><p>Project Started</p></div>
-                  <div className="chartObject" style={{width: "test"}}><p>Updates</p></div>
+
+
+                  <div>
+                    <p className="mb-2 pb-0">Projects Launched or Completed /month</p>
+                    <div className="bar-graph">
+                      {this.projectData.map((data) => {
+                        return <div className="bar" style={{width: 5 + 495*(data/10)}} />
+                      })}
+                      <p className="mb-0 pb-0">0 &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; 3</p>
+                    </div>
+                  </div>
+
+                  <div className="pie-wrapper">
+                    <div className="pie-chart"></div>
+                    <div className="pie-data">
+                      {this.languageData.map((data, key) =>{
+                        var color = "rgb(var(--color" + (key+1);
+                        color+= "))";
+                        var value = {backgroundColor: color};
+                        return <div className="d-flex"> <p>{data.name}: {data.amount}</p> <div id="colorbox" style={value}> </div></div>
+                      })}
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
